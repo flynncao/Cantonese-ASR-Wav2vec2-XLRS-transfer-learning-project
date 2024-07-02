fb9f42e5e0c9bf040f4afb5d6c20f3e013fa4c5e
Erin-lab-design <q.li.32@student.rug.nl>
2024-07-02 17:49:01 +0200

    Delete baseline model directory

--- baseline model/trad2simp.py
+++
@@ -1,6 +0,0 @@
-from opencc import OpenCC
-
-def convert_to_simplified(text: str) -> str:
-    converter = OpenCC('t2s')  # t2s 表示从繁体转到简体
-    simplified_text = converter.convert(text)
-    return simplified_text
