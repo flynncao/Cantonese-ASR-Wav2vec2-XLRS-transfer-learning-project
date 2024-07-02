3742b2769347e386747e888731e0c248ad8d4517
Erin-lab-design <q.li.32@student.rug.nl>
2024-07-02 17:49:25 +0200

    Delete joint model experiment directory

--- joint model experiment/bert_model.py
+++
@@ -1,5 +0,0 @@
-from transformers import BertModel, BertConfig, AutoTokenizer
-
-def load_bert_model(model_path, config_path):
-    model = BertModel.from_pretrained(model_path, config=config_path)
-    return model
